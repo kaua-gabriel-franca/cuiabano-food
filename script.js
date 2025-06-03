d4e73f9ae0ee295c231f67f77108bb57f3f92722
kaua-gabriel-franca <kaua.gabriel.dev@gmail.com>
2025-06-03 11:19:42 -0400

    add fix.js

--- script.js
+++ script.js
@@ -1,61 +1,60 @@
-$(document).ready(function(){
-    $('mobile_btn').on('click', function(){
-        $('"mobile_menu').toggleClass('atctive')
-        $('"mobile_menu').find('i').toggleClass('fa-x')
+$(document).ready(function () {
+  $("#mobile_btn").on("click", function () {
+    $("#mobile_menu").toggleClass("active");
+    $("#mobile_btn").find("i").toggleClass("fa-x");
+  });
+
+  const sections = $("section");
+  const navItems = $(".nav-item");
+
+  $(window).on("scroll", function () {
+    const header = $("header");
+    const scrollPosition = $(window).scrollTop() - header.outerHeight();
+    let activeSectionIndex = 0;
+
+    if (scrollPosition <= 0) {
+      header.css("box-shadow", "none");
+    } else {
+      header.css("box-shadow", "5px 1px 5px rgba(0, 0, 0, 0.1)");
+    }
+
+    sections.each(function (i) {
+      const section = $(this);
+      const sectionTop = section.offset().top - 96;
+      const sectionBottom = sectionTop + section.outerHeight();
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+        activeSectionIndex = i;
+        return false;
+      }
     });
 
-    const sections = $('section');
-    const navItems = $('.nav-item');
+    navItems.removeClass("active");
+    $(navItems[activeSectionIndex]).addClass("active");
+  });
 
-    $(window).on('scrool', function(){
-        const header = $('header');
-        const scroolPosition = $(window).scroolTop() - header.outerHeight();
-        let activeSectionIndex = 0;
-        
-        if(scroolPosition <= 0) {
-            header.css('box-shadow', 'none');
-        } else {
-            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
-        }
+  /** Animação */
+  ScrollReveal().reveal("#cta", {
+    origin: "left",
+    duration: 2000,
+    distance: "20%",
+  });
 
-        sections.each(function(i){
-            const section = $(this);
-            const sectinTop = section.offset().top - 96;
-            const sectionBottom = sectionTop + section.outerHeight();
+  ScrollReveal().reveal(".dish", {
+    origin: "left",
+    duration: 2000,
+    distance: "20%",
+  });
 
-            if(scroolPosition >= sectionTop && scroolPosition < sectionBottom) {
-                activeSectionIndex = i;
-                return false;
-            } 
-        })
-        navItems.removeClass ('active');
-        $(navItems[activeSectionIndex]).addClass('active');
-    });
+  ScrollReveal().reveal("#testimonial_dish", {
+    origin: "left",
+    duration: 1000,
+    distance: "20%",
+  });
 
-    /** A animação */
-    ScrollReveal().reveal('#cta', {
-        origin: 'left',
-        duration: 2000,
-        distance: '20%'
-    })
-
-    ScrollReveal().reveal(".dish", {
-      origin: "left",
-      duration: 2000,
-      distance: "20%",
-    });
-
-    ScrollReveal().reveal("#testimonials_dish", {
-      origin: "left",
-      duration: 2000,
-      distance: "20%",
-    });
-
-    ScrollReveal().reveal("#feedbacks", {
-      origin: "right",
-      duration: 2000,
-      distance: "20%",
-    });
-
-
+  ScrollReveal().reveal(".feedback", {
+    origin: "right",
+    duration: 1000,
+    distance: "20%",
+  });
 });
