e1a1f9120400ed49bd853fecfa40ef1e081ab3b7
kaua-gabriel-franca <kaua.gabriel.dev@gmail.com>
2025-06-05 07:26:02 -0400

    add link nas redes sociais

--- script.js
+++ script.js
@@ -40,6 +40,12 @@ $(document).ready(function () {
     distance: "20%",
   });
 
+  ScrollReveal().reveal("#banner", {
+    origin: "right",
+    duration: 2000,
+    distance: "20%",
+  });
+
   ScrollReveal().reveal(".dish", {
     origin: "left",
     duration: 2000,
